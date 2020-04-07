9092b39e77a8491b2c73d3ac70d25ed2aebb14cb
Steven Blasiol <blasiols@oregonstate.edu>
2020-04-07 18:05:17 +0000

    added fake menu for project plan

---
+++ menu.py
@@ -0,0 +1,24 @@
+import shutil
+import os
+
+cols, rows = shutil.get_terminal_size()
+
+lastLine = rows//2
+centerLeftRight = cols//1
+centerTopBottom = (lastLine) // 3 
+
+os.system('clear')
+
+print('\n' * centerTopBottom)
+print('The Spook Mansion Mystery'.center(centerLeftRight, ' '))
+print('\n')
+print('Do you dare enter the Mansion?'.center(centerLeftRight, ' '))
+print('\n')
+print('Please Make a Selection:'.center(centerLeftRight, ' '))
+print('\n')
+print('\'startgame\' - to start a new game'.center(centerLeftRight, ' ')) 
+print('\'loadgame\' - to load a saved game'.center(centerLeftRight, ' '))
+print('\'credits\' - to view the game credits'.center(centerLeftRight, ' '))
+print('\'exit\' - to exit the game'.center(centerLeftRight, ' '))
+print('\n' * lastLine)
+selection = input('Enter selection:')
