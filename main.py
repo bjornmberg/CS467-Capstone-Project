00dcc6fc92fbf9efe83aa2499262aee26739a050
snakesonaguy <snakesonaguy@users.noreply.github.com>
2020-04-13 20:55:37 -0400

    changed room structure to single class

--- main.py
+++ main.py
@@ -1,40 +1,36 @@
-from Room import Parlor, Library, Solarium, GameRoom, DSBathroom, \
-    DiningRoom, Foyer, Kitchen, LinenCloset, RedRoom, \
-    PinkRoom, SecFloorLanding, SecFloorBathroom, GreenRoom, Attic, \
-    Cellar, ServQuarters, ServBathroom, Crypt, Gazebo, \
-    RoseGarden, Porch, FrontLawns
+from Room import Room
 
 
 # Just initializing all of the Rooms to default values
-parlor = Parlor(False, [])
-library = Library(False, [])
-solarium = Solarium(False, [])
-gameRoom = GameRoom(False, [])
-dsBathroom = DSBathroom(False, [])
-diningRoom = DiningRoom(False, [])
-foyer = Foyer(False, [])
-kitchen = Kitchen(False, [])
-linenCloset = LinenCloset(False, [])
-redRoom = RedRoom(False, [])
-pinkRoom = PinkRoom(False, [])
-secFloorLanding = SecFloorLanding(False, [])
-secFloorBathroom = SecFloorBathroom(False, [])
-greenRoom = GreenRoom(False, [])
-attic = Attic(False, [])
-cellar = Cellar(False, [])
-servQuarters = ServQuarters(False, [])
-servBathroom = ServBathroom(False, [])
-crypt = Crypt(False, [])
-gazebo = Gazebo(False, [])
-roseGarden = RoseGarden(False, [])
-porch = Porch(False, [])
-frontLawns = FrontLawns(False, [])
+parlor = Room("Parlor Long", "Parlor Short", False, [])
+library = Room("Library Long", "Library Short",False, [])
+solarium = Room("Solarium Long", "Solarium Short",False, [])
+gameRoom = Room("Game Room Long", "Game Room Short",False, [])
+dsBathroom = Room("Downstairs Bathroom Long", "Downstairs Bathroom Short",False, [])
+diningRoom = Room("Dining Room Long", "Dining Room Short",False, [])
+foyer = Room("Foyer Long", "Foyer Short",False, [])
+kitchen = Room("Kitchen Long", "Kitchen Short",False, [])
+linenCloset = Room("Linen Closet Long", "Linen Closet Short",False, [])
+redRoom = Room("Red Room Long", "Red Room Short",False, [])
+pinkRoom = Room("Pink Room Long", "Pink Room Short",False, [])
+secFloorLanding = Room("Landing Long", "Landing Short",False, [])
+secFloorBathroom = Room("Upstairs Bathroom Long", "Upstairs Bathroom Short",False, [])
+greenRoom = Room("Green Room Long", "Green Room Short",False, [])
+attic = Room("Attic Long", "Attic Short",False, [])
+cellar = Room("Cellar Long", "Cellar Short",False, [])
+servQuarters = Room("Servant Quarters Long", "Servant Quarters Short",False, [])
+servBathroom = Room("Servant Bathroom Long", "Servant Bathroom Short", False, [])
+crypt = Room("Crypt Long", "Crypt Short", False, [])
+gazebo = Room("Gazebo Long", "Gazebo Short", False, [])
+roseGarden = Room("Rose Garden Long", "Rose Garden Short", False, [])
+porch = Room("Porch Long", "Porch Short", False, [])
+frontLawns = Room("Front Lawns Long", "Front Lawns Short", False, [])
 
 # calling the linking function to point all of the Rooms at each other
 # this only represents the first floor right now, for testing
 frontLawns.linkRooms(porch, None, None, None, None, None)
 porch.linkRooms(foyer, frontLawns, None, None, None, None)
-foyer.linkRooms(dsBathroom, Porch, parlor, library, secFloorLanding, None)
+foyer.linkRooms(dsBathroom, porch, parlor, library, secFloorLanding, None)
 library.linkRooms(gameRoom, None, foyer, None, None, None)
 gameRoom.linkRooms(solarium, library, dsBathroom, None, None, None)
 solarium.linkRooms(None, gameRoom, None, None, None, None)
