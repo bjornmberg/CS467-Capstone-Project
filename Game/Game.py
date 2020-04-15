4b4b644cd209fd5547ee297b17e71fed174710e8
snakesonaguy <snakesonaguy@users.noreply.github.com>
2020-04-15 14:41:09 -0400

    refactoring to PEP8

--- Game/Game.py
+++ Game/Game.py
@@ -5,11 +5,11 @@ from Hero import Hero
 
 class Game:
 
-    roomsList = list()
+    rooms_list = list()
 
     # This function will display the credits for the Game
     # Needs formatting
-    def displayCredits(self):
+    def display_credits(self):
         # os.system('cls')
         print('THIS IS WHERE THE CREDITS WILL BE!')
         input('PRESS ANY KEY AND ENTER TO RETURN')
@@ -18,7 +18,7 @@ class Game:
     # This function displays the menu and asks the user for input
     # The input (string) is returned to the calling function
     # Needs formatting
-    def displayMenu(self):
+    def display_menu(self):
 
         # os.system('cls')
 
@@ -37,103 +37,103 @@ class Game:
     def start(self):
 
         while 1:
-            selection =  self.displayMenu()
+            selection =  self.display_menu()
             if selection == 'newgame':
-                self.playGame('datastore\\newGame.json')
+                self.play_game('datastore\\newGame.json')
             elif selection == 'loadgame':
-                self.playGame('datastore\\saveGame.json')
+                self.play_game('datastore\\saveGame.json')
             elif selection == 'credits':
-                self.displayCredits()
+                self.display_credits()
             elif selection == 'exit':
                 break
 
     # This function is used to initialize the Room objects to a state from either
     # a save file or a start file.
-    def initializeRooms(self, data):
+    def initialize_rooms(self, data):
 
         # iterate through the roomData and initialize Room objects for each item
         for x in data:
             # initialize the object
-            newRoom = Room(x['name'], x['longDes'], x['shortDes'], x['visited'], x['roomId'])
+            new_room = Room(x['name'], x['longDes'], x['shortDes'], x['visited'], x['roomId'])
             # copy the droppedItems from the roomData to the object's droppedItems
-            newRoom.droppedItems = x['droppedItems'].copy()
+            new_room.dropped_items = x['droppedItems'].copy()
             # copy the linkedRooms from the roomData to the object's linkedRooms
-            newRoom.linkedRooms = x['linkedRooms'].copy()
+            new_room.linked_rooms = x['linkedRooms'].copy()
             # put the room into the roomsList at the index of its id - example: parlor's id == 0
             # so the parlor room is at roomsList[0]
-            self.roomsList.insert(newRoom.roomId, newRoom)
+            self.rooms_list.insert(new_room.room_id, new_room)
 
         # go through the rooms and link them
-        for y in self.roomsList:
+        for y in self.rooms_list:
             # the linkedRooms list of each item will contain the index of the other rooms
             # it is linked to in the following order north, south, east, west, up, down
             # these values will either be the index of the room in the rooms list (if
             # a link exists, or None if there is no link in that direction
-            n = self.roomsList[y.linkedRooms[0]] if y.linkedRooms[0] is not None else None
-            s = self.roomsList[y.linkedRooms[1]] if y.linkedRooms[1] is not None else None
-            e = self.roomsList[y.linkedRooms[2]] if y.linkedRooms[2] is not None else None
-            w = self.roomsList[y.linkedRooms[3]] if y.linkedRooms[3] is not None else None
-            u = self.roomsList[y.linkedRooms[4]] if y.linkedRooms[4] is not None else None
-            d = self.roomsList[y.linkedRooms[5]] if y.linkedRooms[5] is not None else None
+            n = self.rooms_list[y.linked_rooms[0]] if y.linked_rooms[0] is not None else None
+            s = self.rooms_list[y.linked_rooms[1]] if y.linked_rooms[1] is not None else None
+            e = self.rooms_list[y.linked_rooms[2]] if y.linked_rooms[2] is not None else None
+            w = self.rooms_list[y.linked_rooms[3]] if y.linked_rooms[3] is not None else None
+            u = self.rooms_list[y.linked_rooms[4]] if y.linked_rooms[4] is not None else None
+            d = self.rooms_list[y.linked_rooms[5]] if y.linked_rooms[5] is not None else None
             # call to link the rooms
-            y.linkRooms(n, s, e, w, u, d)
+            y.link_rooms(n, s, e, w, u, d)
 
     # This function is the main game driver function
-    def playGame(self, inputFile):
+    def play_game(self, inputFile):
 
-        gameFile = open(inputFile, 'r')
-        fileData = json.loads(gameFile.read())
-        roomData = fileData['rooms']
+        game_file = open(inputFile, 'r')
+        file_data = json.loads(game_file.read())
+        room_data = file_data['rooms']
 
-        self.initializeRooms(roomData)
+        self.initialize_rooms(room_data)
 
-        heroData = fileData['hero']
-        hero = Hero(heroData['name'], heroData['location'])
+        hero_data = file_data['hero']
+        hero = Hero(hero_data['name'], hero_data['location'])
 
         while 1:
             print('--------------------------------------')
-            currentRoom = self.roomsList[hero.location]
-            print('{}\'s Current Location: {}'.format(hero.name, currentRoom.name))
-            print('Description: {}'.format(currentRoom.getDescription()))
-            print('Linked Room Indices: {}'.format(currentRoom.linkedRooms))
+            current_room = self.rooms_list[hero.location]
+            print('{}\'s Current Location: {}'.format(hero.name, current_room.name))
+            print('Description: {}'.format(current_room.get_description()))
+            print('Linked Room Indices: {}'.format(current_room.linked_rooms))
             direction = input('Enter n, s, e, w, u, d: ')
 
             if direction == 'n':
-                if currentRoom.north is not None:
-                    hero.location = currentRoom.north.roomId
+                if current_room.north is not None:
+                    hero.location = current_room.north.room_id
                 else:
                     print('No Room in that direction')
                     continue
             elif direction == 's':
-                if currentRoom.south is not None:
-                    hero.location = currentRoom.south.roomId
+                if current_room.south is not None:
+                    hero.location = current_room.south.room_id
                 else:
                     print('No Room in that direction')
                     continue
             elif direction == 'e':
-                if currentRoom.east is not None:
-                    hero.location = currentRoom.east.roomId
+                if current_room.east is not None:
+                    hero.location = current_room.east.room_id
                 else:
                     print('No Room in that direction')
                     continue
             elif direction == 'w':
-                if currentRoom.west is not None:
-                    hero.location = currentRoom.west.roomId
+                if current_room.west is not None:
+                    hero.location = current_room.west.room_id
                 else:
                     print('No Room in that direction')
                     continue
             elif direction == 'u':
-                if currentRoom.up is not None:
-                    hero.location = currentRoom.up.roomId
+                if current_room.up is not None:
+                    hero.location = current_room.up.room_id
                 else:
                     print('No Room in that direction')
                     continue
             elif direction == 'd':
-                if currentRoom.down is not None:
-                    hero.location = currentRoom.down.roomId
+                if current_room.down is not None:
+                    hero.location = current_room.down.room_id
                 else:
                     print('No Room in that direction')
                     continue
 
-            currentRoom.setVisited()
+            current_room.set_visited()
 
--- Room/Room.py
+++ Room/Room.py
@@ -6,36 +6,36 @@ class Room:
     west = None             # Room to the West
     up = None               # Room upstairs
     down = None             # Room downstairs
-    droppedItems = list()   # list of items dropped in this room
-    linkedRooms = list()    # list of the indexes of linkedRooms
+    dropped_items = list()   # list of items dropped in this room
+    linked_rooms = list()    # list of the indexes of linkedRooms
 
     # Initializer for the Room class
-    def __init__(self, name, longDes, shortDes, visited, roomId):
+    def __init__(self, name, long_des, short_des, visited, room_id):
         self.name = name
-        self.longDes = longDes
-        self.shortDes = shortDes
+        self.long_des = long_des
+        self.short_des = short_des
         self.visited = visited
-        self.roomId = roomId
+        self.room_id = room_id
 
 
     # This function prints the description of the Room based on whether it has been visited or not
-    def getDescription(self):
+    def get_description(self):
         if not self.visited:
-            return(self.longDes)
+            return(self.long_des)
         else:
             # print the shortDes if the Room has been visited and print any items dropped in the room
-            if len(self.droppedItems) > 0:
-                return("{}\nYou have left these items here: {}".format(self.shortDes, self.droppedItems))
+            if len(self.dropped_items) > 0:
+                return("{}\nYou have left these items here: {}".format(self.short_des, self.dropped_items))
             else:
-                return(self.shortDes)
+                return(self.short_des)
 
     # This function will need to be toggled when a player enters the Room (after calling the getDescription function)
-    def setVisited(self):
+    def set_visited(self):
         self.visited = True
 
     # this function will link the rooms together, it takes room objects in the various
     # directions and links them
-    def linkRooms(self, north, south, east, west, up, down):
+    def link_rooms(self, north, south, east, west, up, down):
         self.north = north
         self.south = south
         self.east = east
