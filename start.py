95c86719cefa626dcab50e03b871f31dc4972334
Bjorn M. Berg <bjornmberg@gmail.com>
2020-04-21 18:34:42 -0700

    Add system call & rename resizeUtility to start

--- start.py
+++ start.py
@@ -1,3 +1,5 @@
+import os
 # The following uses code adapted from:
 # https://stackoverflow.com/questions/16941885/want-to-resize-terminal-windows-in-python-working-but-not-quite-right
 print("\x1b[8;40;100t")
+os.system("python3 main.py")
