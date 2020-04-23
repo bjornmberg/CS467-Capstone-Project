a1674ec36d8afd7ce874fe08bb019505dec97d8b
Bjorn M. Berg <bjornmberg@gmail.com>
2020-04-23 11:59:22 -0700

    Add minimal structure to inventoryMapScreen.py

--- inventoryMapScreen/inventoryMapScreen.py
+++ inventoryMapScreen/inventoryMapScreen.py
@@ -1,1 +1,16 @@
-print('toDo')
+import os
+
+def display():
+    selection = -1
+
+    while selection != ' ':
+        os.system('clear')
+
+
+
+
+        selection = input('press \'enter\' to return to the game... ')
+        selection = ' ' + selection
+
+    # Having reached this point, selection matches. Clear screen to get ready to return to the game
+    os.system('clear')
