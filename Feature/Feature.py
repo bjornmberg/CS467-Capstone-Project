c6e77f8fb31a9a3e8d5a8bc741c7a3fb5f759285
Steven Blasiol <blasiols@oregonstate.edu>
2020-05-07 15:20:09 -0400

    PEP8 Feature.py

--- Feature/Feature.py
+++ Feature/Feature.py
@@ -1,28 +1,46 @@
-'''
-Implementation of the Feature class. A feature is an object that exists within a room (example: couch, drawer, etc).
-There are three descriptions of the Feature:
-    pre_action_des: used when 'look'-ing and the Feature prior to interaction
-    in_action_des: used when interacting with the Feature
-    post_action_des: used when the feature can no longer be interacted with
-    Example:
-        pre_action: "The couch's cushions look lumpy there might be something there..."
-        in_action: "You see a key lying where the cushions were..."
-        post_action: "This is the couch where you found the key..."
-
-    The Feature's state is what dictates the above behavior:
-        0 - pre_action
-        1 - in_action
-        3 - post_action
-
-    The feature_id is an integer that is used to place the Feature into the feature list() within
-    the Room object at a specified location. This is important for interactions as Items can be tied
-    to the Feature and they need to know which Feature to modify
-'''
 class Feature:
+    """ Class used to represent a static feature with a Room
 
-    # constructor for the Feature class
+    Attributes
+    ----------
+    name: str
+        the name of the Feature
+    pre_action_des: str
+        describes the Feature prior to interaction
+    in_action_des: str
+        describes the Feature during interaction
+    post_action_des: str
+        describes the Feature after interaction
+    actionable: bool
+        denotes whether a Feature can be interacted with
+    usable: bool
+        denotes whether an Item can be used on a Feature
+    state: int
+        manages the state of the Feature
+    feature_id: int
+        unique identifier of the Feature within the Room
+
+    Methods
+    -------
+    get_description():
+        returns the appropriate description based on Feature state
+    save_feature()
+        returns dictionary representation of the Feature for saving
+    """
+
     def __init__(self, name, pre_action_des, in_action_des, post_action_des, actionable, usable, state, feature_id):
 
+        """Constructor for the Feature class
+
+        :param str name: the name of the Feature
+        :param str pre_action_des: the Features pre-action description
+        :param str in_action_des: the Features in-action description
+        :param str post_action_des: the Features post-action description
+        :param bool actionable: denotes ability to interact with Feature
+        :param bool usable: denotes ability to use Item on Feature
+        :param int state: denotes state of the Feauture
+        :param int feature_id: unique identifier of Feature within Room
+        """
         self.name = name
         self.pre_action_des = pre_action_des
         self.in_action_des = in_action_des
@@ -35,10 +53,11 @@ class Feature:
     def __repr__(self):
         return self
 
-
-    # checks the state of the feature and returns the appropriate description
     def get_description(self):
+        """ Provides the description based on Feature state
 
+        :return: str : description of the Feature based on state
+        """
         if self.state == 0:
             return self.pre_action_des
         elif self.state == 1:
@@ -47,8 +66,12 @@ class Feature:
             return self.post_action_des
 
     def save_feature(self):
+        """Provides dict representation of the Feature for saving
+
+        :return: dict : used for saving the Feature
+        """
         feature_dict = {
-            'name':self.name,
+            'name': self.name,
             'preActionDes': self.pre_action_des,
             'inActionDes': self.in_action_des,
             'postActionDes': self.post_action_des,
