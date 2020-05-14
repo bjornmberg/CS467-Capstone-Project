27217be72cda2e07c88302d2d304326ef212af6c
Bjorn M. Berg <bjornmberg@gmail.com>
2020-05-13 18:46:39 -0700

    Add own "text-wrapper" for highlighting text

--- Room/Room.py
+++ Room/Room.py
@@ -236,19 +236,27 @@ class Room:
 
         # If not visited, check for newlines and output long description
         if not self.visited:
-            # description += self.long_des
-            if '\n' in self.long_des:
-                list_lines = self.long_des.splitlines()
-                print()
-                for x in list_lines:
-                    print(textwrap.fill('{}'.format(x), 100, initial_indent=(' ' * 20), subsequent_indent=(' ' * 20)))
-            # Else player has visited before, output short description
+
+            # Check for newlines & bold or character signifiers
+            if ('\n' in self.long_des) or ('@' in self.long_des) or ('^' in self.long_des) or ('#' in self.long_des):
+                processed = self.wrap_processor(self.long_des)
+                for i in processed:
+                    print(i)
+
+            # Else no newlines or bold color signifiers. Simple processing.
             else:
                 print(textwrap.fill('{}'.format(self.long_des), 100, initial_indent=(' ' * 20), subsequent_indent=(' ' * 20)))
+        # Else player has visited before, output short description
         else:
-            wrapped_text = textwrap.wrap(self.short_des, width=83)
-            for i in wrapped_text:
-                print((' ' * 20) + i)
+            # Check for newlines & bold or character signifiers
+            if ('\n' in self.short_des) or ('@' in self.short_des) or ('^' in self.short_des) or ('#' in self.short_des):
+                processed = self.wrap_processor(self.short_des)
+                for i in processed:
+                    print(i)
+
+            # Else no newlines or bold color signifiers. Simple processing.
+            else:
+                print(textwrap.fill('{}'.format(self.short_des), 100, initial_indent=(' ' * 20), subsequent_indent=(' ' * 20)))
 
         print()
         print(textwrap.fill('YOU CAN \'move\': ', initial_indent=(' ' * 20)))
@@ -313,3 +321,84 @@ class Room:
                 return status, item
         else:
             return status, item
+
+
+    def wrap_processor(self, string):
+        """Processes input string and outputs wrapped as TextWrapper does, without stripping special chars
+
+        :param string: String for wrapping
+        :return: Wrapped string
+        """
+        # Establish variables for use in processing
+        wrapped = ''
+        list_for_processing = []
+        counter = 0
+        width = 80
+
+        mid_processing_list = []
+
+        # If newline, split lines and process
+        if '\n' in string:
+            splitLinesList = string.splitlines()
+
+            # For each line in array. The following is adapted from a post on
+            # https://stackoverflow.com/questions/16430200/a-good-way-to-make-long-strings-wrap-to-newline
+            for x in splitLinesList:
+                # Split each by word and process
+                list_for_processing = x.split()
+                wrapped = '                    '
+                counter = 0
+                for word in range(0, len(list_for_processing)):
+                    # If the current word is of length that can be added to the current line, within width limit
+                    if counter + len(list_for_processing[word]) + 1 <= width:
+                        # Add the word to the wrapped string
+                        wrapped = wrapped + list_for_processing[word] + ' '
+                        # Increment the counter
+                        counter = counter + len(list_for_processing[word]) + 1
+                    else:
+                        wrapped = wrapped + '\n' + '                    ' + list_for_processing[word] + ' '
+                        counter = len(list_for_processing[word])
+                mid_processing_list.append(wrapped)
+
+        # Else, process a string without newlines
+        # The following is adapted from a post on:
+        # https://stackoverflow.com/questions/16430200/a-good-way-to-make-long-strings-wrap-to-newline
+        else:
+            list_for_processing = string.split()
+            wrapped = '                    '
+            counter = 0
+            for word in range(0, len(list_for_processing)):
+                # If the current word is of length that can be added to the current line, within width limit
+                if counter + len(list_for_processing[word]) + 1 <= width:
+                    # Add the word to the wrapped string
+                    wrapped = wrapped + list_for_processing[word] + ' '
+                    # Increment the counter
+                    counter = counter + len(list_for_processing[word]) + 1
+                else:
+                    # Add a newline, and the word in question. Increment counter
+                    wrapped = wrapped + '\n' + '                    ' + list_for_processing[word] + ' '
+                    counter = len(list_for_processing[word])
+            # Add the string to the list for later processing
+            mid_processing_list.append(wrapped)
+
+        # Perform character substitution, replacing special marker characters with terminal commands
+        final_list = []
+        final_string = ''
+        for x in mid_processing_list:
+            for z in x:
+                if z == '@':
+                    # Bold Red Text
+                    new_char = '\033[1;31m'
+                elif z == '^':
+                    # Bold Green Text
+                    new_char = '\033[1;32m'
+                elif z == '#':
+                    # End terminal command, reset to normal
+                    new_char = '\033[0m'
+                else:
+                    new_char = z
+                final_string = final_string + new_char
+            final_list.append(final_string)
+            final_string = ''
+
+        return final_list
