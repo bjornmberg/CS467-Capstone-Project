fd7c80b6e085819fb47bed299e108428c2dafb82
Bjorn M. Berg <bjornmberg@gmail.com>
2020-05-21 21:03:28 -0700

    Fix bug 10-match intro screen formatting w in-game

--- Intro/intro.py
+++ Intro/intro.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from Wrapper import wrapper
 
 # Initialize some variables for use in displaying the credits screen
 cols, rows = shutil.get_terminal_size()
@@ -14,29 +15,24 @@ def display():
 
     :return: VOID
     """
+
+    intro_string = 'It is a ^crisp# fall day. You\'re walking along a dirt trail about a half hour out from your new hometown in Pembroke Falls, Maine. Your feet crunch on fallen leaves. The sun is starting to go down and you really should get back, you think to yourself. You haven\'t explored this trail before and you would not want to get lost.\n\n\nYou think you hear a stick cracking, but it was not due to you.\n\n\n What was that?            ...            Maybe there is someone else here?\n\n\nYou look around but see nothing. Or you think you saw nothing. There was a blur by a tree behind you, but that must just be a bird... right?\n\nYou hear a sound like a woosh. You  see a flash of light as you are struck from behind. You fall forward and all goes black.\n\n'
+
     selection = -1
     while selection != ' ':
         os.system('clear')
         print ('█' * cols)
         print('\n' * centerTopBottom)
         # Print a top border to the box
-        longestString = 'and you really should get back, you think to yourself. You haven\'t'
-        print(('▒' * len(longestString)).center(centerLeftRight))
+        print((' ' * 20) + ('▒' * 85) + '\n')
         print('\n')
-        print('It is a crisp fall day. You\'re walking along a dirt trail about'.center(centerLeftRight))
-        print('a half hour out from your new hometown in Pembroke Falls, Maine.'.center(centerLeftRight))
-        print('Your feet crunch on fallen leaves. The sun is starting to go down'.center(centerLeftRight))
-        print(longestString.center(centerLeftRight))
-        print('explored this trail before and you would not want to get lost.\n'.center(centerLeftRight))
-        print('You think you hear a stick cracking, but it was not due to you.\n\n'.center(centerLeftRight))
-        print('What was that? Maybe there is someone else here?\n'.center(centerLeftRight))
-        print('You look around but see nothing. Or you think you saw nothing.'.center(centerLeftRight))
-        print('There was a blur by a tree behind you, but must be a bird...\n\n'.center(centerLeftRight))
-        print('You hear a sound like a woosh. You  see a flash of light as'.center(centerLeftRight))
-        print('you are struck from behind. You fall forward and all goes black.\n\n'.center(centerLeftRight))
-        print(('▒' * len(longestString)).center(centerLeftRight))
+        processed = wrapper.wrap_processor(intro_string)
+        for i in processed:
+            print(i)
+        print('\n')
+        print((' ' * 20) + ('▒' * 85) + '\n')
 
-        print('\n' * (lastLine - 10))
+        print('\n' * (lastLine - 14))
         selection = input('press \'enter\' to continue... ')
         selection = ' ' + selection
     # Having reached this point, selection matches. Clear screen to begin game.
