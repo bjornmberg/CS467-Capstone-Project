634c80a3b91bba9169a7d05b6f83e7ab9d24db1e
Steven Blasiol <blasiols@oregonstate.edu>
2020-05-22 13:26:55 -0400

    fixed bug 34

--- languageParser/languageParser.py
+++ languageParser/languageParser.py
@@ -55,7 +55,7 @@ class LanguageParser:
                              "right gargoyle", "paint", "music box", "bed", "rocking horse", "rose", "spade", 
                              "fountain", "roses", "hair", "door lock", "shelf", "toilet", "sink", "mirror", "journal", 
                              "locket", "vine", "window", "statue", "tile", "hollow", "grave", "girl", "lock", 
-                             "paintbrush", "grill", "tub", "windows", "chairs", "tables", "small bed", "drawers"]
+                             "paintbrush", "grill", "tub", "windows", "chairs", "tables", "small bed", "drawers", "books"]
 
         self.tw_look_objects = ["window", "sill", "east", "window", "west", "south", "pry", "bar", "pad", "lock",
                                 "undead", "chef", "grave", "tree", "book", "case", "north", "pocket", "watch", "left",
